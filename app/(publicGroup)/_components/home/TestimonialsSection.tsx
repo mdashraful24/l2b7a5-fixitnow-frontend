0f1c5e546df359ef619843fb6e9dc6c58bef9aec
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-08-02 02:42:45 +0600

    added updated home page section responsiveness done

--- app/(publicGroup)/_components/home/TestimonialsSection.tsx
+++ app/(publicGroup)/_components/home/TestimonialsSection.tsx
@@ -37,7 +37,7 @@ const TestimonialsSection = () => {
                     </p>
                 </div>
 
-                <div className="grid md:grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {testimonials.map((testimonial, index) => (
                         <div
                             key={index}
